88ef4a486cd6affa8f4feb884c91156743bc5b5e
baotran1311 <baotran13112003@gmail.com>
2024-05-29 22:33:37 +0700

    security rules _ add env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,6 +11,17 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyDFE_E8kIxFpZRKaFSelpRA79xvl_yYfiw",
   authDomain: "insta-clone-48dad.firebaseapp.com",
@@ -21,6 +32,7 @@ const firebaseConfig = {
   measurementId: "G-64W7WKBNQG",
 };
 
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
